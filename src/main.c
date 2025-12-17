e93a4539382c02bcf5192f2f6ffc153c3caa470d
ByteGab <gabrielfreiresimon@gmail.com>
2025-12-17 13:44:59 +0100

    refactor/ main

--- src/main.c
+++ src/main.c
@@ -6,29 +6,12 @@
 /*   By: gafreire <gafreire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/10 16:49:46 by gafreire          #+#    #+#             */
-/*   Updated: 2025/12/17 11:33:44 by gafreire         ###   ########.fr       */
+/*   Updated: 2025/12/17 13:41:42 by gafreire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <string.h>
-#include "libft.h"
+#include "cubed.h"
 
-
-int check_map(char *map)
-{
-    int len;
-
-    len = ft_strlen(map); // change function
-    if (len > 4)
-    {
-        if (ft_strncmp(map + (len - 4),".cub",4) != 0 && strncmp(map + (len - 4),".xpm",4) != 0 )//change function
-            return (printf("File invalid\n"),-1);
-    }
-    else
-        return (printf("Not valid map\n"),-1);
-    return (0);
-}
 
 int main (int argc, char *argv[])
 {
@@ -36,7 +19,7 @@ int main (int argc, char *argv[])
         printf("Error arguments\n");
     else
     {
-        if (check_map(argv[1]) != -1)
+        if (check_map(argv[1]))
         {
             //  if (parse_cub(argv[1])) // change name
             //  {
