fc65c0a2fdf92f05821a4eda380e5a93acf6877e
Gabriel Dan Omandam <gomandam@c1r10s1.42madrid.com>
2026-01-22 06:20:15 +0100

    [add] dda steps

--- src/raycast/dda_step.c
+++ src/raycast/dda_step.c
@@ -6,8 +6,47 @@
 /*   By: gomandam <gomandam@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/15 21:02:27 by gomandam          #+#    #+#             */
-/*   Updated: 2026/01/15 21:07:26 by gomandam         ###   ########.fr       */
+/*   Updated: 2026/01/22 06:19:38 by gomandam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 // steps, perform DDA, check for hits. Distance ray travels between grid lines
+
+#include "../../include/cubed.h"
+
+static int	check_wall_hit(t_game *game, t_raycast *ray)
+{
+	if (ray->map_y < 0 || ray->map_y >= game->map_height)
+		return (1);
+	if (ray->map_x < 0 || ray->map_x >= (int)ft_strlen(game->map[ray->map_y]))
+		return (1);
+	if (game->map[ray->map_y][ray->map_x] == '1')
+		return (1);
+	return (0);
+}
+
+static void	step_ray(t_raycast *ray)
+{
+	if (ray->side_dist_x < ray->side_dist_y)
+	{
+		ray->side_dist_y += ray->delta_dist_x;
+		ray->map_x += ray->step_x;
+		ray->side = 0;
+	}
+	else
+	{
+		ray->side_dist_y += ray->delta_dist_y;
+		ray->map_y += ray->step_y;
+		ray->side = 1;
+	}
+}
+
+void	perform_dda(t_game *game, t_raycast *ray)
+{
+	while (ray->hit == 0)
+	{
+		step_ray(ray);
+		if (check_wall_hit(game, ray))
+			ray->hit = 1;
+	}
+}
--- src/raycast/init_ray.c
+++ src/raycast/init_ray.c
@@ -6,21 +6,22 @@
 /*   By: gomandam <gomandam@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/15 21:02:09 by gomandam          #+#    #+#             */
-/*   Updated: 2026/01/22 04:51:27 by gomandam         ###   ########.fr       */
+/*   Updated: 2026/01/22 06:06:40 by gomandam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 // intialize ray and calculate distance 
 
-#include "cubed.h"
+#include "../../include/cubed.h"
 
 void	init_ray_direction(t_game *game, t_raycast *ray, int x)
 {
 	ray->camera_x = 2.0 * x / (double)game->gfx.width - 1.0;
-	ray->dir_x = game->player.dir_x 
-	ray->dir_y
-	ray->map_x
-	ray->map_y
+	ray->dir_x = game->player.dir_x + game->player.plane_x * ray->camera_x;
+	ray->dir_y = game->player.dir_y + game->player.player_y * ray->camera_x;
+// scalar value camera_x axis rays casted horizontally across the screen L to R
+	ray->map_x = (int)game->player.pos_x;
+	ray->map_y = (int)game->player.pos_y;
 	ray->hit = 0;
 }
 
