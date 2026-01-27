d214e6ff2dff7c27ba677ba294d2b1395439a6ab
Gabriel Dan Omandam <gomandam@c3r1s1.42madrid.com>
2026-01-27 03:49:18 +0100

    [add] struct

--- src/raycast/init_image.c
+++ src/raycast/init_image.c
@@ -6,7 +6,7 @@
 /*   By: gomandam <gomandam@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/23 02:03:48 by gomandam          #+#    #+#             */
-/*   Updated: 2026/01/23 03:09:58 by gomandam         ###   ########.fr       */
+/*   Updated: 2026/01/27 03:43:56 by gomandam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,14 +22,14 @@ void	init_image(t_game *game)
 		free_resources(game);
 		exit(1);
 	}
-	game->image.addr = mlx_get_data_addr(game->iamge.img,
+	game->image.addr = mlx_get_data_addr(game->image.img,
 			&game->image.bits_per_pixel,
 			&game->image.line_length,
 			&game->image.endian);
 }
 // update minilibx directory
 
-void	put_pixel(t_img *image, int x, int y,)
+void	put_pixel(t_image *image, int x, int y,)
 {
 	char	*pixel;
 	int		offset;
