abf1532a3d648a9073760dbb38e86b42d74ad75c
ByteGab <gabrielfreiresimon@gmail.com>
2026-02-05 17:56:51 +0100

    feature/ add comments textures

--- src/graphics/textures.c
+++ src/graphics/textures.c
@@ -6,11 +6,18 @@
 /*   By: gafreire <gafreire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/02 18:40:01 by gafreire          #+#    #+#             */
-/*   Updated: 2026/02/04 16:07:21 by gafreire         ###   ########.fr       */
+/*   Updated: 2026/02/05 17:55:44 by gafreire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cubed.h"
+
+/*
+	Auxiliary function:
+		1. Load XPM file into image
+		2. Get pixel data address
+		3. If fails, free and exit
+*/
 
 static void	load_texture(t_game *game, t_texture *tex, char *path)
 {
@@ -26,6 +33,12 @@ static void	load_texture(t_game *game, t_texture *tex, char *path)
 			&tex->line_length, &tex->endian);
 }
 
+/*
+	Auxiliary function:
+		1. Load all four wall textures
+		2. Store them in textures array (N, S, E, W)
+*/
+
 void	init_textures(t_game *game)
 {
 	load_texture(game, &game->textures[TEX_N], game->tex_north);
@@ -33,6 +46,13 @@ void	init_textures(t_game *game)
 	load_texture(game, &game->textures[TEX_E], game->tex_east);
 	load_texture(game, &game->textures[TEX_W], game->tex_west);
 }
+
+/*
+	Auxiliary function:
+		1. Check if coordinates are within texture bounds
+		2. Calculate pixel position in memory
+		3. Return color value (0 if out of bounds)
+*/
 
 int	get_texture_color(t_texture *tex, int x, int y)
 {
