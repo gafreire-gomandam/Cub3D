52d4eec59f4fbc6496f5cbe61db6c0896277949c
ByteGab <gabrielfreiresimon@gmail.com>
2026-02-05 18:16:37 +0100

    fix/ add comments parser map

--- src/parser/parser_map.c
+++ src/parser/parser_map.c
@@ -6,11 +6,18 @@
 /*   By: gafreire <gafreire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/17 12:54:10 by gafreire          #+#    #+#             */
-/*   Updated: 2026/01/04 11:46:30 by gafreire         ###   ########.fr       */
+/*   Updated: 2026/02/05 18:12:34 by gafreire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cubed.h"
+
+/*
+	Auxiliary function:
+		1. Check if filename length is greater than 4
+		2. Verify last 4 characters are ".cub"
+		3. Return 0 if invalid, 1 if valid
+*/
 
 static int	check_file(char *map)
 {
@@ -27,6 +34,13 @@ static int	check_file(char *map)
 	return (1);
 }
 
+/*
+	Auxiliary function:
+		1. Try to open the file in read-only mode
+		2. If open fails, return 0
+		3. Close file descriptor and return 1 if successful
+*/
+
 static int	check_info(char *map)
 {
 	int	fd;
@@ -37,6 +51,13 @@ static int	check_info(char *map)
 	close(fd);
 	return (1);
 }
+
+/*
+	Main function:
+		1. Validate file has .cub extension
+		2. Verify file can be opened
+		3. Return 0 if any check fails, 1 if all pass
+*/
 
 int	check_map(char *map)
 {
